a8c9d497f782862e0ff6afeaa6384aa2d390dc4e
taneataylor-AP <54554538+taneataylor-AP@users.noreply.github.com>
2019-12-06 16:55:50 -0500

    Update app.js

--- public/app.js
+++ public/app.js
@@ -29,9 +29,9 @@ function ytubeStudy() {
 }
 
 function createCards() {
-    window.location.href = "/QnA/flashcards/index.html";
+    window.location.href = "/QnA/public/flashcards/index.html";
 }
 
 function viewCards() {
-    window.location.href = '/QnA/flashcards/index.html';
+    window.location.href = '/QnA/public/flashcards/index.html';
 }
